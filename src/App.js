e76dbfc694f3310444de2321d4f7e6adfc2cafaa
martinezrafael <rafaelmartinez.contato@gmail.com>
2026-01-14 17:56:39 -0300

    Cria uma lista de menu com map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
 import Logotype from "./components/Logotype";
 
+const textoOpcoes = ["Categorias", "Favoritos", "Minha Estante"];
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Logotype />
+        <ul className="opcoes">
+          {textoOpcoes.map((texto, index) => {
+            return <li className={`opcao opcao-${index + 1}`}>{texto}</li>;
+          })}
+        </ul>
       </header>
     </div>
   );
