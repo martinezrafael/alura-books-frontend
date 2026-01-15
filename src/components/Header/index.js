d0e6d32b5e6fdeaf57ce19aad47f7e772e07ac8f
martinezrafael <rafaelmartinez.contato@gmail.com>
2026-01-15 16:32:17 -0300

    cria o headerContainer com styled-components

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,13 +3,22 @@ import OpcoesHeader from "../OpcoesHeader";
 import IconesHeader from "../IconesHeader";
 import "./style.css";
 
+import styled from "styled-components";
+
+const HeaderContainer = styled.header`
+  background: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+`;
+
 function Header() {
   return (
-    <header className="App-header">
+    <HeaderContainer>
       <Logotype />
       <OpcoesHeader />
       <IconesHeader />
-    </header>
+    </HeaderContainer>
   );
 }
 
