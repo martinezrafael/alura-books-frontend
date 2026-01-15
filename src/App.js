29acea59863b50da02121d4a6eb8ddf503891f06
martinezrafael <rafaelmartinez.contato@gmail.com>
2026-01-15 16:25:40 -0300

    cria o AppContainer com o styled-components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import "./App.css";
 import Header from "./components/Header";
 
+import styled from "styled-components";
+
+const AppContainer = styled.div`
+  background-image: linear-gradient(90deg, #002f52 35%, #326589);
+  height: 100vh;
+  width: 100vw;
+`;
+
 function App() {
   return (
-    <div className="App">
+    <AppContainer>
       <Header />
-    </div>
+    </AppContainer>
   );
 }
 
